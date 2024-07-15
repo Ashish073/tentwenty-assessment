75cdd4e911b985d9e3c224622ef6579af11f70de
Ashish Karapurker <38071797+Ashish073@users.noreply.github.com>
2024-07-15 08:09:46 +0530

    fix: test for production

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,9 @@ module.exports = {
     main: "./src/index.js",
   },
   output: {
-    static: {
-      directory: path.resolve(__dirname, "public"),
-    },
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
-    publicPath: "/",
+    publicPath: path.resolve(__dirname, "public"),
     clean: true,
   },
   module: {
