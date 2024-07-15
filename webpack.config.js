0cf627886363cab76a5f87ea16cb3274cbb9f508
Ashish Karapurker <38071797+Ashish073@users.noreply.github.com>
2024-07-15 08:13:32 +0530

    fix: revert all webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
-    publicPath: "./public",
+    publicPath: "/",
     clean: true,
   },
   module: {
